4db9987fca9b5202cc516a640a0066afef32a376
vibe <vitaliy.beletskiy@gmail.com>
2026-09-22 16:49:35 +0200

    Initial Spring Boot project

---
+++ src/test/java/vibe/roombooking/BookingApplicationTests.java
@@ -0,0 +1,13 @@
+package vibe.roombooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
